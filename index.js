cb327d6fc38255d53645314280fa41889a3cbe28
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2017-12-20 10:01:04 -0800

    debugging cors deploy

--- index.js
+++ index.js
@@ -60,7 +60,11 @@ app.use(
     // bucket: 'platypost',
     region: 'us-west-1', //optional
     signatureVersion: 'v4', //optional (use for some amazon regions: frankfurt and others)
-    // headers: { 'Access-Control-Allow-Origin': '*' }, // optional
+    headers: {
+      TEMPXYZ: 'Did this go through',
+      'Access-Control-Allow-Origin':
+        'http://platypost.s3-website-us-west-1.amazonaws.com'
+    }, // optional
     ACL: 'private', // this is default
     uniquePrefix: true // (4.0.2 and above) default is true, setting the attribute to false preserves the original filename in S3
   })
