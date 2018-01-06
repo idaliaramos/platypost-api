9dc18179afda0822168043ed2de2c8da784d8f96
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-01-06 14:53:07 -0800

    deploy2

--- index.js
+++ index.js
@@ -58,14 +58,13 @@ app.use(
   '/s3',
   require('react-s3-uploader/s3router')({
     bucket: 'mails110017',
-    // bucket: 'platypost',
     region: 'us-west-1', //optional
     signatureVersion: 'v4', //optional (use for some amazon regions: frankfurt and others)
     headers: {
       TEMPXYZ: 'Did this go through',
       'Access-Control-Allow-Origin':
         'http://platypost.s3-website-us-west-1.amazonaws.com'
-    }, // optional
+    }, // optional fix put env variables
     ACL: 'private', // this is default
     uniquePrefix: true // (4.0.2 and above) default is true, setting the attribute to false preserves the original filename in S3
   })
